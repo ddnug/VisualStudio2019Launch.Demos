569ac7cfc956ba4b335508835b50f9b89238716b
Scott Dockendorf <scott@dockendorf.org>
2019-05-09 06:39:58 -0500

    Initial commit of C# 8 Demo 1

---
+++ src/CSharp8Demo1/CSharp8.0/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+//#nullable enable
+
+namespace CSharp8._0
+{
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public Person(string firstName, string middleName, string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subscribers = Service.GetSubscribers();
+            var names = GetNames(subscribers);
+
+            foreach (var name in names)
+            {
+                Console.WriteLine($"{name} has subscribed to the service");
+            }
+        }
+
+        static IEnumerable<string> GetNames(IEnumerable<Person> people)
+        {
+            foreach (var p in people)
+            {
+                yield return GetName(p);
+            }
+        }
+
+        static string GetName(Person p)
+        {
+            return $"{p.FirstName} {p.MiddleName[0]}. {p.LastName}";
+        }
+
+        #region async
+        //static async Task Main(string[] args)
+        //{
+        //    var subscribers = Service.GetSubscribersAsync();
+        //    var names = GetNames(subscribers);
+
+        //    await foreach (var name in names)
+        //    {
+        //        Console.WriteLine($"{name} has subscribed to the service");
+        //    }
+        //}
+
+        //static async IAsyncEnumerable<string> GetNames(IAsyncEnumerable<Person> people)
+        //{
+        //    await foreach (var p in people)
+        //    {
+        //        yield return GetName(p);
+        //    }
+        //}
+        #endregion
+
+
+    }
+}
